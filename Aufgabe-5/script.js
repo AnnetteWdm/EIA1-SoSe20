88cda3d8dc1ba4e2484da31810238d51dc2a2d52
Annette WIedenmann <rina.chick@web.de>
2020-05-27 14:39:03 +0200

    Aufgabe 5 Grundgerüst

---
+++ Aufgabe-5/script.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Emission absolute of Europe in 2018" + 4.209, 3);
+console.log("Ralative to totals world´s emission" + 4);
+console.log("Growth rate between 2008 and 2018 (in %)" + 1.2);
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe-5/script.ts
@@ -0,0 +1,7 @@
+
+
+console.log ("Emission absolute of Europe in 2018" + 4.209,3) ;
+
+console.log ("Ralative to totals world´s emission" + 4) ;
+
+console.log ( "Growth rate between 2008 and 2018 (in %)" + 1.2 ) ;
