e892bb153424b8a861ae64c2ec7d40f93c452174
Annette WIedenmann <rina.chick@web.de>
2020-07-03 17:25:46 +0200

    Spracherkennung

--- Aufgabe-10/script.ts
+++ Aufgabe-10/script.ts
@@ -230,3 +230,45 @@ function deleteTodo(index: number): void {
      */
     drawListToDOM();
 }
+
+declare var Artyom: any;
+
+window.addEventListener("load", function(): void {
+    const artyom: any = new Artyom();
+    
+    artyom.addCommands({
+        indexes: ["erstelle Aufgabe *"],
+        smart: true,
+        action: function( i: any, wildcard: string): void {
+            console.log("Neue Aufgabe wird erstellt: " + wildcard);
+           
+            todoObjects.unshift({ //Mit unshift wird Array am Anfang eingefügt//
+                todosText: wildcard,
+                todosChecked: false
+                });
+            
+        }
+
+    });
+    
+    function startContinuousArtyom(): void {
+        artyom.fatality();
+    
+        setTimeout(
+            function(): void {
+                artyom.initialize({
+                    lang: "de-DE",
+                    continuous: true,
+                    listen: true,
+                    interimResults: true,
+                    debug: true
+                }).then(function(): void {
+                    console.log("Ready!");
+                });
+            }, 
+            250);
+    }
+    
+    startContinuousArtyom();
+    
+});
